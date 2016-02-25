69d75db644f84e4e0c2c0726aeab8e2336bcacbd
frogfrogfrog <421604505@qq.com>
2016-02-25 14:38:03 +0800

    add #21 merge two sorted list

--- src/easy/MergeTwoSortedLists.java
+++ src/easy/MergeTwoSortedLists.java
@@ -11,6 +11,35 @@ public class MergeTwoSortedLists {
 	}
 
 	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-		return null;
+		if(l1==null)
+			return l2;
+		if(l2==null)
+			return l1;
+		ListNode start;
+		ListNode cur;
+		if(l2.val<l1.val){
+			start=cur=l2;
+			l2=l2.next;
+		}
+		else{
+			start=cur=l1;
+			l1=l1.next;
+		}
+		while(l1!=null && l2!=null){
+			if(l2.val<l1.val){
+				cur.next=l2;
+				cur=cur.next;
+				l2=l2.next;
+			}else{
+				cur.next=l1;
+				cur=cur.next;
+				l1=l1.next;
+			}
+		}
+		if(l1!=null)
+			cur.next=l1;
+		if(l2!=null)
+			cur.next=l2;
+		return start;
 	}
 }
