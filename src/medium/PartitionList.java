2eae0356fbad6f64ed302b162a61e18e66ee527e
frogfrogfrog <421604505@qq.com>
2016-03-01 18:04:43 +0800

    add #86

---
+++ src/medium/PartitionList.java
@@ -0,0 +1,24 @@
+package medium;
+
+public class PartitionList {
+	public ListNode partition(ListNode head, int x) {
+		ListNode smallHead=new ListNode(0);
+		ListNode bigHead=new ListNode(0);
+		ListNode cur=head,small=smallHead,big=bigHead;
+		while(cur!=null){
+			ListNode next=cur.next;
+			if(cur.val<x){
+				small.next=cur;
+				small=cur;
+				small.next=null;
+			}else{
+				big.next=cur;
+				big=cur;
+				big.next=null;
+			}
+			cur=next;
+		}
+		small.next=bigHead.next;
+		return smallHead.next;
+	}
+}
