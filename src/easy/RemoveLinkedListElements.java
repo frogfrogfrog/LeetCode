d751f8d08b18876ee4fbacadf1d2135854cfd88a
frogfrogfrog <421604505@qq.com>
2016-03-01 14:21:03 +0800

    add #2 #203

---
+++ src/easy/RemoveLinkedListElements.java
@@ -0,0 +1,30 @@
+package easy;
+
+public class RemoveLinkedListElements {
+	public class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) {
+			val = x;
+		}
+	}
+
+	public ListNode removeElements(ListNode head, int val) {
+		if(head==null)
+			return head;
+		ListNode stub=new ListNode(0);
+		stub.next=head;
+		ListNode prev=stub;
+		ListNode cur=head;
+		while(cur!=null){
+			if(cur.val==val){
+				prev.next=cur.next;
+				cur=cur.next;
+			}else{
+				prev=cur;
+				cur=cur.next;
+			}
+		}
+		return stub.next;
+	}
+}
---
+++ src/medium/AddTwoNumbers.java
@@ -0,0 +1,41 @@
+package medium;
+
+public class AddTwoNumbers {
+	public class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) {
+			val = x;
+		}
+	}
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		int carry=0;
+		ListNode cur=new ListNode(0);
+		ListNode head=cur;
+		while(l1!=null || l2!=null){
+			int temp=0;
+			if(l1!=null)
+				temp+=l1.val;
+			if(l2!=null)
+				temp+=l2.val;
+			temp+=carry;
+			ListNode newNode=new ListNode(temp);
+			carry=0;
+			if(newNode.val>=10){
+				newNode.val=newNode.val-10;
+				carry=1;
+			}
+			if(l1!=null)
+				l1=l1.next;
+			if(l2!=null)
+				l2=l2.next;
+			cur.next=newNode;
+			cur=newNode;
+		}
+		if(carry!=0){
+			ListNode newNode=new ListNode(1);
+			cur.next=newNode;
+		}
+		return head.next;
+	}
+}
