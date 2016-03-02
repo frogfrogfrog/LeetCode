8dcf6adacd0d4cf07918c8e6dd7937c794718959
frogfrogfrog <421604505@qq.com>
2016-03-02 16:33:30 +0800

    add #237

---
+++ src/easy/DeleteNodeInALinkedList.java
@@ -0,0 +1,14 @@
+package easy;
+
+public class DeleteNodeInALinkedList {
+	public void deleteNode(ListNode node) {
+		ListNode cur = node;
+		ListNode prev = null;
+		while (cur.next != null) {
+			cur.val = cur.next.val;
+			prev=cur;
+			cur = cur.next;
+		}
+		prev.next=null;
+	}
+}
