7edafa190429a832145f31eeb33fa8a8456b903f
frogfrogfrog <421604505@qq.com>
2016-03-21 18:52:52 +0800

    add #26

---
+++ src/easy/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,16 @@
+package easy;
+
+public class RemoveDuplicatesFromSortedArray {
+	public int removeDuplicates(int[] nums) {
+		int size=nums.length;
+		if(size<2){
+			return size;
+		}
+		int res=1;
+		for(int i=0;i<size-1;i++){
+			if(nums[i]!=nums[i+1])
+				nums[res++]=nums[i+1];
+		}
+		return res;
+    }
+}
