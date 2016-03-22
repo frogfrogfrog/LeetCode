02bdc4ccfbbae34fcbd02085b7406a70a83574a8
frogfrogfrog <421604505@qq.com>
2016-03-22 15:20:35 +0800

    add #283

---
+++ src/easy/MoveZeroes.java
@@ -0,0 +1,14 @@
+package easy;
+
+public class MoveZeroes {
+	public void moveZeroes(int[] nums) {
+		int size=nums.length;
+		int count=0;
+		for(int i=0;i<size;i++){
+			if(nums[i]!=0)
+				nums[count++]=nums[i];
+		}
+		for(int i=count;i<size;i++)
+			nums[i]=0;
+	}
+}
