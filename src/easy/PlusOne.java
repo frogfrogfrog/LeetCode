950f7477a81401ddecee751cb4a46e7a4c3abca4
frogfrogfrog <421604505@qq.com>
2016-03-28 14:03:10 +0800

    add #290

---
+++ src/easy/PlusOne.java
@@ -0,0 +1,28 @@
+package easy;
+
+public class PlusOne {
+	public int[] plusOne(int[] digits) {
+		int carry=1;
+		int pos=digits.length-1;
+		while(carry!=0 && pos>=0){
+			int temp=digits[pos]+carry;
+			if(temp<10)
+				carry=0;
+			digits[pos]=temp%10;
+			pos--;
+		}
+		if(carry>0){
+			int[] a=new int[digits.length+1];
+			System.arraycopy(digits, 0, a, 1, digits.length);
+			a[0]=carry;
+			return a;
+		}else{
+			return digits;
+		}
+    }
+	
+	public static void main(String[] args){
+		int[] a=new int[]{9};
+		new PlusOne().plusOne(a);
+	}
+}
---
+++ src/easy/WordPattern.java
@@ -0,0 +1,26 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordPattern {
+	public boolean wordPattern(String pattern, String str) {
+		char[] patternList=pattern.toCharArray();
+		String[] strList=str.split(" ");
+		if(patternList.length!=strList.length)
+			return  false;
+		Map<Character,String> map=new HashMap<Character,String>();
+		int size=patternList.length;
+		for(int i=0;i<size;i++){
+			if(map.containsKey(patternList[i])){
+				if(!map.get(patternList[i]).equals(strList[i]))
+					return false;
+			}else{
+				if(map.containsValue(strList[i]))
+					return false;
+				map.put(patternList[i], strList[i]);
+			}
+		}
+		return true;
+	}
+}
