78f65633ec3d0c957cbfde95cf3bdcd2ac9bfea4
frogfrogfrog <421604505@qq.com>
2016-04-04 16:04:41 +0800

    add #231

---
+++ src/easy/PowerOfTwo.java
@@ -0,0 +1,10 @@
+package easy;
+
+public class PowerOfTwo {
+	public boolean isPowerOfTwo(int n) {
+		return ((n & n - 1) == 0 && n > 0);
+    }
+	public static void main(String[] args){
+		System.out.println(3&2);
+	}
+}
