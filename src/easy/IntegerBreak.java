aba1bd1de8adfd603dca4478c44098c65bd3efee
frogfrogfrog <421604505@qq.com>
2016-04-24 14:55:48 +0800

    add #343

---
+++ src/easy/IntegerBreak.java
@@ -0,0 +1,19 @@
+package easy;
+
+public class IntegerBreak {
+	public int integerBreak(int n) {
+        int res=1;
+        if(n<=3)
+        	return n-1;
+        while(n>0){
+        	if(n>4 || n==3){
+        		n-=3;
+        		res*=3;
+        	}else{
+        		n-=2;
+        		res*=2;
+        	}
+        }
+        return res;
+    }
+}
