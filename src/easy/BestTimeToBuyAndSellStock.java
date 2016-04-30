218b31e09cfdf6081a007ed0cc0ea6f2f4082424
frogfrogfrog <421604505@qq.com>
2016-04-30 18:15:05 +0800

    add #121

---
+++ src/easy/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,18 @@
+package easy;
+
+public class BestTimeToBuyAndSellStock {
+	public int maxProfit(int[] prices) {
+		int size=prices.length;
+        if(size==0)
+        	return 0;
+        int maxProfit=0;
+        int sofarMin=prices[0];
+        for(int i=0;i<size;i++){
+        	if(prices[i]<sofarMin)
+        		sofarMin=prices[i];
+        	else
+        		maxProfit=Math.max(maxProfit, prices[i]-sofarMin);
+        }
+        return maxProfit;
+    }
+}
