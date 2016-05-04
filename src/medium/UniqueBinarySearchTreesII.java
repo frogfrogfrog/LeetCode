851c7ed0cdc75c868eccd89c8be111c167014793
frogfrogfrog <421604505@qq.com>
2016-05-04 18:46:55 +0800

    add #95

---
+++ src/medium/UniqueBinarySearchTreesII.java
@@ -0,0 +1,33 @@
+package medium;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class UniqueBinarySearchTreesII {
+	public List<TreeNode> generateTrees(int n) {
+		if(n==0){
+			return new LinkedList<TreeNode>();
+		}
+        return generate(1, n);
+    }
+	public List<TreeNode> generate(int start,int end) {
+        List<TreeNode> res=new LinkedList<TreeNode>();
+        if(start>end){
+        	res.add(null);
+        	return res;
+        }
+        for(int i=start;i<=end;i++){
+        	List<TreeNode> leftTrees=generate(start, i-1);
+        	List<TreeNode> rightTrees=generate(i+1, end);
+        	for(TreeNode left:leftTrees){
+        		for(TreeNode right:rightTrees){
+        			TreeNode root=new TreeNode(i);
+        			root.left=left;
+        			root.right=right;
+        			res.add(root);
+        		}
+        	}
+        }
+        return res;
+    }
+}
