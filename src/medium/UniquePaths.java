91fa697c84dd1e93f1465e70f74ab8b378e8b4e1
frogfrogfrog <421604505@qq.com>
2016-05-05 11:16:56 +0800

    add #62 dp solution

--- src/medium/UniquePaths.java
+++ src/medium/UniquePaths.java
@@ -15,6 +15,20 @@ public class UniquePaths {
     public int uniquePaths(int m, int n) {
         return comb(m + n - 2, n-1);
     }
+    
+    
+    public int uniquePathsDP(int m, int n) {
+        int[][] dp=new int[m][n];
+        for(int i=0;i<m;i++){
+        	for(int j=0;j<n;j++){
+        		if(i==0 || j==0)
+        			dp[i][j]=1;
+        		else
+        			dp[i][j]=dp[i][j-1]+dp[i-1][j];
+        	}
+        }
+        return dp[m-1][n-1];
+    }
     public static void main(String[] args){
     	new UniquePaths().uniquePaths(51,9);
     }
