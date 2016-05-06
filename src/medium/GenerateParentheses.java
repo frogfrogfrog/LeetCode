d00e621312f198cd287ef8f6b2dd283d6209962d
frogfrogfrog <421604505@qq.com>
2016-05-06 12:53:41 +0800

    add #22

---
+++ src/medium/GenerateParentheses.java
@@ -0,0 +1,25 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParentheses {
+	private List<String> res = new ArrayList<String>();
+
+	public List<String> generateParenthesis(int n) {
+		this.generate("", n, 0);
+		return this.res;
+	}
+
+	public void generate(String tmp, int left, int right) {
+		if (left == 0 && right == 0)
+			res.add(tmp);
+		if (left > 0)
+			generate(tmp + "(", left - 1, right + 1);
+		if (right > 0)
+			generate(tmp + ")", left, right - 1);
+	}
+	public static void main(String[] args){
+		new GenerateParentheses().generateParenthesis(3);
+	}
+}
