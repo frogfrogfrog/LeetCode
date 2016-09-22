bb282b8ad4e327cb4da1547b327144ea1ababc1b
frogfrogfrog <421604505@qq.com>
2016-09-22 23:06:46 +0800

    add #383

---
+++ src/easy/RansomNote.java
@@ -0,0 +1,15 @@
+package easy;
+
+public class RansomNote {
+	public boolean canConstruct(String ransomNote, String magazine) {
+        int[] countM=new int[26];
+        for(char a:magazine.toCharArray()){
+        	countM[a-'a']++;
+        }
+        for(char a:ransomNote.toCharArray()){
+        	if(--countM[a-'a']<0)
+        		return false;
+        }
+        return true;
+    }
+}
