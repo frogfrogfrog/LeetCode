95bc3d2712b7e5ce8262af444a38c4ef74e33457
frogfrogfrog <421604505@qq.com>
2016-09-25 22:34:42 +0800

    add #387

---
+++ src/easy/FirstUniqueCharacterInAString.java
@@ -0,0 +1,38 @@
+package easy;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class FirstUniqueCharacterInAString {
+	public int firstUniqChar(String s) {
+		Map<Character,Integer> map=new LinkedHashMap<Character,Integer>();
+		Set<Character> set=new HashSet<Character>();
+		char[] chars=s.toCharArray();
+		for(int i=0;i<chars.length;i++){
+			if(map.containsKey(chars[i])){
+				map.remove(chars[i]);
+				set.add(chars[i]);
+			}
+			if(!set.contains(chars[i]))
+				map.put(chars[i], i);
+		}
+		if(map.entrySet().size()==0)
+			return -1;
+		Iterator<Integer> it=map.values().iterator();
+		return it.next();
+    }
+	public static void main(String[] args){
+		Map<Integer,Character> map=new LinkedHashMap<Integer,Character>();
+		map.put(1, 'z');
+		map.put(0, 'd');
+		map.put(2, 'c');
+		for(Entry<Integer, Character> d:map.entrySet()){
+			System.out.println(d.getKey());
+			System.out.println(d.getValue());
+		}
+	}
+}
