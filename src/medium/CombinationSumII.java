80047977b44ee9fc51f1b9e94785f7186854b9be
frogfrogfrog <421604505@qq.com>
2016-09-25 23:51:15 +0800

    add #40

--- src/medium/CombinationSumII.java
+++ src/medium/CombinationSumII.java
@@ -1,17 +1,18 @@
 package medium;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class CombinationSum {
+public class CombinationSumII {
 	List<List<Integer>> res = new ArrayList<List<Integer>>();
-
-	public List<List<Integer>> combinationSum(int[] candidates, int target) {
+	public List<List<Integer>> combinationSum2(int[] candidates, int target) {
 		res.clear();
+		Arrays.sort(candidates);
 		this.findASolution(candidates, new ArrayList<Integer>(), target, 0, 0);
 		return res;
-	}
-
+    }
+	
 	private void findASolution(int[] candidates, ArrayList<Integer> ans, int target, int sum, int start) {
 		if (sum > target)
 			return;
@@ -19,18 +20,19 @@ public class CombinationSum {
 			res.add((List<Integer>) ans.clone());
 		else {
 			for (int i = start; i < candidates.length; i++) {
+				if(i>start && candidates[i]==candidates[i-1])
+					continue;
 				sum += candidates[i];
 				ans.add(candidates[i]);
-				findASolution(candidates, ans, target, sum, i);
+				findASolution(candidates, ans, target, sum, i+1);
 				sum -= candidates[i];
 				ans.remove(ans.size() - 1);
 			}
 		}
 	}
-	
 	public static void main(String[] args){
-		CombinationSum cs=new CombinationSum();
-		cs.combinationSum(new int[]{2,2,2,3,6,7}, 7);
+		CombinationSumII cs=new CombinationSumII();
+		cs.combinationSum2(new int[]{1,10,1,2,7,6,1,5}, 8);
 		for(int i=0;i<cs.res.size();i++){
 			
 			System.out.println(cs.res.get(i));
