36ac327f4861f554262a4790e64e60625db5344d
frogfrogfrog <421604505@qq.com>
2016-09-26 21:10:42 +0800

    add #20

---
+++ src/easy/ValidParentheses.java
@@ -0,0 +1,26 @@
+package easy;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+	public boolean isValid(String s) {
+		Stack<Character> stack = new Stack<Character>();
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if (!stack.isEmpty() && ((c == ')' && stack.peek() == '(') || (c == ']' && stack.peek() == '[')
+					|| (c == '}' && stack.peek() == '{'))) {
+				stack.pop();
+			} else {
+				stack.push(c);
+			}
+		}
+		if (stack.isEmpty())
+			return true;
+		else
+			return false;
+	}
+
+	public static void main(String[] args) {
+		new ValidParentheses().isValid("()");
+	}
+}
