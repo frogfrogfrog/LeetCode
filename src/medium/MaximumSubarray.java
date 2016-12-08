4186d0a8895f9fe768e35198fe54d3f5a14fa1f1
frogfrogfrog <421604505@qq.com>
2016-12-08 21:20:41 +0800

    add #53

---
+++ src/medium/MaximumSubarray.java
@@ -0,0 +1,19 @@
+package medium;
+
+/**
+ * Created by 42160 on 2016/12/8.
+ */
+public class MaximumSubarray {
+    public int maxSubArray(int[] nums){
+        int max=nums[0];
+        int tmpMax=0;
+        for(int i=1;i<nums.length;i++){
+            tmpMax+=nums[i];
+            if(tmpMax>max)
+                max=tmpMax;
+            if (tmpMax<0)
+                tmpMax=0;
+        }
+        return max;
+    }
+}
