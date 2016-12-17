82fe3f2e30f8681b3a36233da111fce46e46b11e
frogfrogfrog <421604505@qq.com>
2016-12-17 16:14:08 +0800

    add #67

--- src/easy/AddBinary.java
+++ src/easy/AddBinary.java
@@ -7,27 +7,27 @@ public class AddBinary {
     public String addBinary(String a, String b) {
         StringBuilder sb = new StringBuilder();
         int pos1 = a.length() - 1, pos2 = b.length() - 1;
-        boolean flag = false;
-        while (pos1 >= 0 && pos2 >= 0) {
-            if (a.charAt(pos1) == '1' && b.charAt(pos2) == '1') {
-                if (flag == true)
-                    sb.insert(0, '1');
-                else
-                    sb.insert(0, '0');
-            } else if (a.charAt(pos1) == '0' && b.charAt(pos2) == '0') {
-                if (flag == true) {
-                    sb.insert(0, '1');
-                    flag = false;
-                } else
-                    sb.insert(0, '0');
-            } else {
-                if (flag == true)
-                    sb.insert(0, '0');
-                else {
-                    sb.insert(0, '1');
-                }
+        int flag = 0;
+        while (pos1 >= 0 || pos2 >= 0) {
+            int sum = flag;
+            if (pos1 >= 0) {
+                sum += a.charAt(pos1) - '0';
+                pos1--;
             }
+            if (pos2 >= 0) {
+                sum += b.charAt(pos2) - '0';
+                pos2--;
+            }
+            sb.insert(0, sum % 2);
+            flag = sum / 2;
         }
+        if (flag != 0)
+            sb.insert(0, '1');
         return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.print(new AddBinary().addBinary("11", "1"));
+        System.out.println((int) '1' ^ (int) '0' ^ 0);
     }
 }
