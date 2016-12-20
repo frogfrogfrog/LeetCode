87a196b544bf419fc67c445bf76621e33ee88b9b
frogfrogfrog <421604505@qq.com>
2016-12-20 15:43:46 +0800

    add #81

--- src/medium/SearchInRotatedSortedArrayII.java
+++ src/medium/SearchInRotatedSortedArrayII.java
@@ -1,53 +1,51 @@
-package hard;
+package medium;
 
 /**
- * Created by 42160 on 2016/12/19.
+ * Created by 42160 on 2016/12/20.
  */
-public class SearchInRotatedSortedArray {
-    public int search(int[] nums, int target) {
-        int index = -1;
+public class SearchInRotatedSortedArrayII {
+    public boolean search(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
         while (left <= right) {
             int mid = (left + right) / 2;
             if (nums[mid] == target)
-                return mid;
-            if (nums[left] <= nums[mid]) {
+                return true;
+            if (nums[left] < nums[mid]) {
                 if (target < nums[left] || target > nums[mid])
                     left = mid + 1;
                 else
                     right = mid - 1;
-            } else {
+            } else if (nums[left] > nums[mid]) {
                 if (target < nums[mid] || target > nums[right])
                     right = mid - 1;
                 else
                     left = mid + 1;
+            } else {
+                left++;
             }
         }
-        return index;
+        return false;
     }
 
-    public int searchRecursive(int[] nums, int target) {
+    public boolean searchRecursive(int[] nums, int target) {
         return binarySearch(nums, 0, nums.length - 1, target);
     }
 
-    public int binarySearch(int[] nums, int left, int right, int target) {
+    public boolean binarySearch(int[] nums, int left, int right, int target) {
         if (left > right)
-            return -1;
+            return false;
         if (nums[left] < nums[right] && (target < nums[left] || target > nums[right]))
-            return -1;
+            return false;
         int mid = (right - left) / 2 + left;
         if (nums[mid] == target)
-            return mid;
+            return true;
         if (nums[left] < nums[right]) {
             if (nums[mid] < target)
                 return binarySearch(nums, mid + 1, right, target);
             else
                 return binarySearch(nums, left, mid - 1, target);
         } else {
-            int ans1 = binarySearch(nums, left, mid - 1, target);
-            int ans2 = binarySearch(nums, mid + 1, right, target);
-            int res = ans2 >= ans1 ? ans2 : ans1;
-            return res;
+            return binarySearch(nums, left, mid - 1, target) || binarySearch(nums, mid + 1, right, target);
         }
     }
 }
