025238914eff29f8f899fae0e4b6757ff5c76bd4
frogfrogfrog <421604505@qq.com>
2017-01-02 17:03:07 +0800

    add #45

--- src/hard/JumpGameII.java
+++ src/hard/JumpGameII.java
@@ -8,8 +8,9 @@ public class JumpGameII {
         int size = nums.length, start = 0, end = 0, maxend = 0, step = 0;
         while (end < size - 1) {
             step++;
-            for (int i = start; i < end; i++) {
-                if (nums[i] + i >= size - 1) return step;
+            for (int i = start; i <= end; i++) {
+                if (nums[i] + i >= size - 1)
+                    return step;
                 maxend = Math.max(maxend, nums[i] + i);
             }
             start = end + 1;
